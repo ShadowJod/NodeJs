c1dd8c830de68b201f9bc9a65ac9651ae0f27ada
ShadowJod <nikhilmishra0187@gmail.com>
2024-11-09 16:31:05 +0530

    My files

---
+++ app.js
@@ -0,0 +1,9 @@
+module.exports = {
+  x: 10,
+  y: 20,
+  z:()=>{
+    console.log("My name is sheel");
+    
+    return 20
+  }
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('./app')
+const color = require('colors')
+var i = 8;
+console.log(i)
+console.log("My Name is Nikhil");
+console.log(app.x,app.y,app.z());
+console.log("Package.json");
+console.log("Hello".red);
+
