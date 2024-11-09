436202ed4b3af938e3367de04fdb9951a56a9680
ShadowJod <nikhilmishra0187@gmail.com>
2024-11-09 22:57:17 +0530

    learning

---
+++ Connection.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql2");
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "student",
+});
+
+module.exports = conn;
+
+// conn.connect((err) => {
+//   if (err) {
+//     console.log("Connection Error:", err.message);
+//   } else {
+//     console.log("Connected");
+//   }
+//   conn.query("Select * from student", (err, res) => {
+//     if (err) {
+//       console.log(err.message);
+//     } else {
+//       console.log(res);
+//     }
+//   });
+// });
+
+// conn.query("select * from student", (err, res) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(res);
+//   }
+// });
--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const color = require('colors')
-var i = 8;
-console.log(i)
-console.log("My Name is Nikhil");
-console.log(app.x,app.y,app.z());
-console.log("Package.json");
-console.log("Hello".red);
-console.log(process);
